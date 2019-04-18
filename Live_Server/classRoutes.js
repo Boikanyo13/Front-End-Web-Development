827d52a3370efe1d72b2906e5554f10e03be40ab
Boikanyo13 <bv.radiokana@gmail.com>
2019-04-19 00:01:59 +0200

    RESTful Interface and Creating forms for POSTs

--- Live_Server/classRoutes.js
+++ Live_Server/classRoutes.js
@@ -17,8 +17,47 @@ router.get('/delete', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'delete.html'))
 })
 
-router.post('/edit', function (req, res) {
+router.get('/edit', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'edit.html'))
+})
+// RESTful interface
+router.get('/api/list', function (req, res) {
+  res.json(classList)
+})
+
+router.get('/api/get/:id', function (req, res) {
+  res.json(classList[req.params.id])
+})
+
+router.post('/api/create', function (req, res) {
+  console.log('creating the following student entry: ', req.body.student)
+  let student = { name: req.body.student, studentNumber: req.body.studentNum, Courses: req.body.courses }
+  classList.push(student)
+  res.redirect(req.baseUrl + '/api/list')
+})
+
+router.post('/api/delete', function (req, res) {
+  if (req.body.deleteSt < classList.length) {
+    console.log('deleting the following student: ', req.body.deleteSt)
+    classList.splice(req.body.deleteSt, 1)
+    res.redirect(req.baseUrl + '/api/list')
+  } else {
+    console.log('The row you entered does not exist!')
+    res.redirect(req.baseUrl + '/delete')
+  }
+})
+
+router.post('/api/edit', function (req, res) {
+  let index = req.body.editStudent.split(',')[0]
+  let newName = req.body.editStudent.split(',')[1]
+
+  if (index < classList.length) {
+    classList[index] = newName
+    res.redirect(req.baseUrl + '/api/list')
+  } else {
+    console.log('The row you entered does not exist!')
+    res.redirect(req.baseUrl + '/edit')
+  }
 })
 
 module.exports = router
--- Live_Server/index.js
+++ Live_Server/index.js
@@ -3,13 +3,20 @@
 let express = require('express')
 let app = express()
 
+// loading body parser
+let bodyParser = require('body-parser')
+
 // loading routers
 let mainRouter = require('./mainRoutes.js')
-let classRouter = require('./classRoutes.js')
+let todoRouter = require('./classRoutes.js')
+
+// tell express to use body parser for JSON and URL encoded form bodies
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // mouting our routers
 app.use('/', mainRouter)
-app.use('/class', classRouter)
+app.use('/todo', todoRouter)
 
 app.listen(3000)
 console.log('Express server running on port 3000')
